7eb75218d2d6d80b10eddbd6ffea29d114f378e3
infinitesearchspace <ben.kraakman.2011@gmail.com>
2014-11-21 11:22:00 +1300

    Working large(16k) texture mapping, with zoom

--- Early Tests/gol-slim.py
+++ Early Tests/gol-slim.py
@@ -4,18 +4,19 @@ import numpy as np
 from pygame.locals import *
 from scipy.misc import lena
 
-SCREEN_SIZE = (800, 800)
+SCREEN_SIZE = (400, 400)
 
 class game:
 	def __init__(self):
 		#Set the initial size for the generative array
-		self.grid_w = 2**12 #12 = 4k, 13 = 8k
+		self.grid_w = 2**7 
 		self.grid_h = self.grid_w 
 		
 		#initialise both buffers to 0 in x,y
 		self.current_grid = np.zeros ((self.grid_w, self.grid_h), int)
 		self.next_grid = np.zeros ((self.grid_w, self.grid_h), int)
 
+	#(x of 3), points to data, 
 	def initTexture(self):
 		#data = np.flipud(lena()) #insert test data
 		
@@ -27,16 +28,15 @@ class game:
 		self.mytexture = gl.glGenTextures(1)
 		
 		#bind? the tex id and some implicit thing?
-		gl.glBindTexture(gl.GL_TEXTURE_3D,self.mytexture)
+		gl.glBindTexture(gl.GL_TEXTURE_2D,self.mytexture)
 
 		# texture mode and parameters controlling wrapping and scaling
-		#gl.glTexEnvf( gl.GL_TEXTURE_ENV, gl.GL_TEXTURE_ENV_MODE, gl.GL_MODULATE )
-		#gl.glTexParameterf( gl.GL_TEXTURE_2D, gl.GL_TEXTURE_WRAP_S, gl.GL_REPEAT )
-		#gl.glTexParameterf( gl.GL_TEXTURE_2D, gl.GL_TEXTURE_WRAP_T, gl.GL_REPEAT )
-		gl.glTexParameterf( gl.GL_TEXTURE_2D, gl.GL_TEXTURE_MAG_FILTER, gl.GL_NEAREST ) #close up
-		gl.glTexParameterf( gl.GL_TEXTURE_2D, gl.GL_TEXTURE_MIN_FILTER, gl.GL_NEAREST ) #Far away
-		#gl.GL_LINEAR
-		#gl.GL_NEAREST
+		gl.glTexEnvf( gl.GL_TEXTURE_ENV, gl.GL_TEXTURE_ENV_MODE, gl.GL_MODULATE )
+		gl.glTexParameterf( gl.GL_TEXTURE_2D, gl.GL_TEXTURE_WRAP_S, gl.GL_REPEAT )
+		gl.glTexParameterf( gl.GL_TEXTURE_2D, gl.GL_TEXTURE_WRAP_T, gl.GL_REPEAT )
+		gl.glTexParameterf( gl.GL_TEXTURE_2D, gl.GL_TEXTURE_MAG_FILTER, gl.GL_LINEAR )
+		gl.glTexParameterf( gl.GL_TEXTURE_2D, gl.GL_TEXTURE_MIN_FILTER, gl.GL_LINEAR )
+
 
 		# map the image data to the texture. note that if the input
 		# type is GL_FLOAT, the values must be in the range [0..1]
@@ -51,16 +51,9 @@ class game:
 		gl.glLoadIdentity()
 
 		#Perspectives/layouts
-		#L,R,B,T,?,?
-		#gl.glOrtho(-0.1, 1.1,	-0.1, 1.1,	 0,1)
-				
-		"""offset = 0.5
-		x1 = offset+1
-		x2 = (x1 - 1) *-1
-		y1 = offset+1
-		y2 = (x1 - 1) *-1
-		gl.glOrtho(x2,x1,y2,y1, 0,1)"""
-
+		#gluPerspective(60.0, float(w)/h, .1, 1000.)
+		gl.glOrtho(0,1,0,1,0,1)
+		
 		#no idea what this is for
 		gl.glMatrixMode(gl.GL_MODELVIEW)
 		gl.glLoadIdentity()
@@ -69,21 +62,13 @@ class game:
 		gl.glEnable(gl.GL_TEXTURE_2D)
 
 
-	def changeOrtho(self, offset):
-		x1 = offset+1
-		x2 = (x1 - 1) *-1
-		y1 = offset+1
-		y2 = (x1 - 1) *-1
-		gl.glOrtho(x2,x1,y2,y1, 0,1)
-
-
 	def doDraw(self,w,h):
 		# draw a quad
 		gl.glBegin( gl.GL_QUADS )
-		gl.glTexCoord2f( 0, 1 );    gl.glVertex3f( 0, 1, self.z )
-		gl.glTexCoord2f( 0, 0 );    gl.glVertex3f( 0, 0, self.z )
-		gl.glTexCoord2f( 1, 0 );    gl.glVertex3f( 1, 0, self.z )
-		gl.glTexCoord2f( 1, 1 );    gl.glVertex3f( 1, 1, self.z )
+		gl.glTexCoord2f( 0, 1 );    gl.glVertex3f( 0, 1, 0 )
+		gl.glTexCoord2f( 0, 0 );    gl.glVertex3f( 0, 0, 0 )
+		gl.glTexCoord2f( 1, 0 );    gl.glVertex3f( 1, 0, 0 )
+		gl.glTexCoord2f( 1, 1 );    gl.glVertex3f( 1, 1, 0 )
 		gl.glEnd(  )
 
 #----------------------################
@@ -115,44 +100,13 @@ if __name__ == "__main__":
 	gameDemo.current_grid = np.where((np.random.randint(6, size=(gameDemo.grid_w, gameDemo.grid_h)))==1, 255, 0)
 
 	#Set & render the initial stage
-	gameDemo.z = 0
 	gameDemo.initStage(*SCREEN_SIZE)
-	gameDemo.changeOrtho(1)
 	gameDemo.initTexture()
 	gameDemo.doDraw(*SCREEN_SIZE)
-
-	myOrtho = 0
-	orthDir = 0.05
 
 	#Let's do this for a while
 	while done==False:
 		
-		#Min, max zoom
-		if myOrtho > 3:
-			orthDir = -0.05
-		if myOrtho < -0.4999:
-			orthDir = 0.0001
-
-		#Controlled inward zooms
-		if orthDir < 0 and myOrtho < -0.3:
-			orthDir = -0.001
-		if orthDir < 0 and myOrtho < -0.475:
-			orthDir = -0.0001
-		if orthDir < 0 and myOrtho < -0.495:
-			orthDir = -0.00005
-
-		if orthDir > 0 and myOrtho > -0.495:
-			orthDir = 0.001
-		if orthDir > 0 and myOrtho > -0.475:
-			orthDir = 0.01
-		if orthDir > 0 and myOrtho > -0.3:
-			orthDir = 0.05
-
-		
-		myOrtho += orthDir		
-		print "Zoom:", myOrtho
-		
-
 		clock.tick (60)
 
 		#GUI Exits
@@ -162,7 +116,7 @@ if __name__ == "__main__":
 		    if event.type == KEYUP and event.key == K_ESCAPE:
 		        done=True                
 
-		"""#Do the conway gruntwork
+		#Do the conway gruntwork
 		for y in range (1, gameDemo.grid_h-1):
 			for x in range (1, gameDemo.grid_w-1):
 			    N = gameDemo.Neighbours (x, y)
@@ -179,14 +133,10 @@ if __name__ == "__main__":
 
 		#Swap the host's conway buffers
 		gameDemo.current_grid = gameDemo.next_grid
-		gameDemo.next_grid = np.zeros ((gameDemo.grid_w, gameDemo.grid_h), int)"""
-
-		
+		gameDemo.next_grid = np.zeros ((gameDemo.grid_w, gameDemo.grid_h), int) 
 
 		#Do a full gl render cycle
-		gl.glClear(gl.GL_COLOR_BUFFER_BIT | gl.GL_DEPTH_BUFFER_BIT)
 		gameDemo.initStage(*SCREEN_SIZE)
-		gameDemo.changeOrtho(myOrtho)
 		gameDemo.initTexture()
 		gameDemo.doDraw(*SCREEN_SIZE)
 
---
+++ Early Tests/gol.py
@@ -0,0 +1,156 @@
+SCREEN_SIZE = (400, 400)
+
+from OpenGL.GL import *
+from OpenGL.GLU import *
+import numpy as np
+from numpy import zeros
+from numpy import ones
+import pygame
+from pygame.locals import *
+from scipy.misc import lena
+import OpenGL.GL as gl
+
+np.set_printoptions(threshold=np.nan)
+
+class game:
+	def __init__(self):
+		self.grid_w = 2**7
+		self.grid_h = self.grid_w
+		np.random.seed(np.random.randint(0,100000))
+		self.current_grid = zeros ((self.grid_w, self.grid_h), int)
+		self.next_grid = zeros ((self.grid_w, self.grid_h), int)
+
+	def initTexture(self):
+		"""
+		init the texture - this has to happen after an OpenGL context
+		has been created
+		"""
+		#data = np.flipud(lena())
+		data = self.current_grid
+		w,h = data.shape
+
+		# generate a texture id, make it current
+		self.mytexture = gl.glGenTextures(1)
+		gl.glBindTexture(gl.GL_TEXTURE_2D,self.mytexture)
+
+		# texture mode and parameters controlling wrapping and scaling
+		gl.glTexEnvf( gl.GL_TEXTURE_ENV, gl.GL_TEXTURE_ENV_MODE, gl.GL_MODULATE )
+		gl.glTexParameterf( gl.GL_TEXTURE_2D, gl.GL_TEXTURE_WRAP_S, gl.GL_REPEAT )
+		gl.glTexParameterf( gl.GL_TEXTURE_2D, gl.GL_TEXTURE_WRAP_T, gl.GL_REPEAT )
+		gl.glTexParameterf( gl.GL_TEXTURE_2D, gl.GL_TEXTURE_MAG_FILTER, gl.GL_LINEAR )
+		gl.glTexParameterf( gl.GL_TEXTURE_2D, gl.GL_TEXTURE_MIN_FILTER, gl.GL_LINEAR )
+
+
+		# map the image data to the texture. note that if the input
+		# type is GL_FLOAT, the values must be in the range [0..1]
+		gl.glTexImage2D(gl.GL_TEXTURE_2D,0,gl.GL_RGB,w,h,0,
+		    gl.GL_LUMINANCE,gl.GL_UNSIGNED_BYTE,data)
+
+#----------------------
+
+
+	def initStage(self,w,h):
+		# set the viewport and projection
+		gl.glViewport(0,0,w,h)
+		gl.glMatrixMode(gl.GL_PROJECTION)
+		gl.glLoadIdentity()
+
+		#gluPerspective(60.0, float(w)/h, .1, 1000.) #conway
+		gl.glOrtho(0,1,0,1,0,1) #tex
+		
+
+		gl.glMatrixMode(gl.GL_MODELVIEW)
+		gl.glLoadIdentity()
+
+		#gl.glClear( gl.GL_COLOR_BUFFER_BIT ) #tex
+
+		# enable textures, bind to our texture
+		gl.glEnable(gl.GL_TEXTURE_2D)	#tex
+		#gl.glBindTexture(gl.GL_TEXTURE_2D,self.mytexture) #tex
+
+		#gl.glColor4f( 1, 1, 1, 1 ) #tex, needed?
+
+#----------------------################
+
+	def doDraw(self,w,h):
+		# draw a quad
+		gl.glBegin( gl.GL_QUADS )
+		gl.glTexCoord2f( 0, 1 );    gl.glVertex3f( 0, 1, 0 )
+		gl.glTexCoord2f( 0, 0 );    gl.glVertex3f( 0, 0, 0 )
+		gl.glTexCoord2f( 1, 0 );    gl.glVertex3f( 1, 0, 0 )
+		gl.glTexCoord2f( 1, 1 );    gl.glVertex3f( 1, 1, 0 )
+		gl.glEnd(  )
+
+		#gl.glDisable(gl.GL_TEXTURE_2D)
+
+
+#----------------------################
+
+
+	# Game of life stuff
+	def Neighbours (self, x_, y_):
+		total = 0
+		for y in range (y_ - 1, y_ + 2):
+		    for x in range (x_ - 1, x_ + 2):
+		        if (x, y) != (x_, y_):
+		            if self.current_grid [y, x] != 0:
+		                total += 1
+		return total
+
+
+
+if __name__ == "__main__":
+	gameDemo = game()
+	pygame.init()
+	screen = pygame.display.set_mode(SCREEN_SIZE, HWSURFACE|OPENGL|DOUBLEBUF)
+
+	clock = pygame.time.Clock()    
+	done = False
+
+	np.random.seed(np.random.randint(0,100000))
+	gameDemo.current_grid = np.where((np.random.randint(6, size=(gameDemo.grid_w, gameDemo.grid_h)))==1, 255, 0)
+
+	gameDemo.initStage(*SCREEN_SIZE)
+	gameDemo.initTexture()
+	gameDemo.doDraw(*SCREEN_SIZE)
+
+	while done==False:
+		
+		clock.tick (60)
+		for event in pygame.event.get():
+		    if event.type == QUIT:
+		        done=True
+		    if event.type == KEYUP and event.key == K_ESCAPE:
+		        done=True                
+
+		#glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		for y in range (1, gameDemo.grid_h-1):
+			for x in range (1, gameDemo.grid_w-1):
+			    N = gameDemo.Neighbours (x, y)
+			    if gameDemo.current_grid[y,x]:
+			        if N < 2:
+			            gameDemo.next_grid [y,x] = 0
+			        elif 4 > N > 1:
+			            gameDemo.next_grid [y,x] = 255
+			        elif N > 3:
+			            gameDemo.next_grid [y,x] = 0
+			    else:
+			        if N == 3:
+			            gameDemo.next_grid [y,x] = 255
+
+		gameDemo.current_grid = gameDemo.next_grid
+		gameDemo.next_grid = zeros ((gameDemo.grid_w, gameDemo.grid_h), int) 
+		#print gameDemo.current_grid
+		#print ""
+
+
+		gameDemo.initStage(*SCREEN_SIZE)
+		gameDemo.initTexture()
+		gameDemo.doDraw(*SCREEN_SIZE)
+
+		pygame.display.flip()
+		
+		
+
+	pygame.quit()
